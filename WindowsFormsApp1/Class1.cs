38b48338abb48c9312f3e9e4ee5fe5559f31ae50
n.vicino <vicino@resolteam.com>
2023-03-24 11:50:25 +0100

    creata classe dbmanager e struttura generale

---
+++ WindowsFormsApp1/Class1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+public class Class1
+{
+	public Class1()
+	{
+
+		Console.WriteLine("CIAO A TUTTI");
+
+    public static class DatabaseManager
+    {
+
+        private static MySqlConnection connection;
+        private static string connectionString = "server=localhost;uid=root;database=databancking";
+
+        static DatabaseManager()
+        {
+            // Aprire la connessione al database all'inizio del programma
+            connection = new MySqlConnection(connectionString);
+            connection.Open();
+        }
+
+        public static MySqlConnection GetConnection()
+        {
+            // Restituire la connessione
+            return connection;
+        }
+
+        public static void CloseConnection()
+        {
+            // Chiudere la connessione al database alla fine del programma
+            connection.Close();
+        }
+
+
+    }
+}
