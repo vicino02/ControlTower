6aff798aa0314e2f1c6665cb5d422db70e0845af
n.vicino <vicino@resolteam.com>
2023-03-27 08:52:12 +0200

    modifiche apportate con l'aggiunta della classe automotive

---
+++ WindowsFormsApp1/FunctionAutomotive.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+public class FunctionAutomotive
+
+{
+	public FunctionAutomotive()
+	{
+
+        int calcolo;
+		string IP_destinazione;
+
+		public void Destination(string destinazione)
+		{
+			int num1;
+			Console.Readline(num1);
+			int num2
+	        Console.Readline(num2)
+            calcolo = num1 + num2 / 2;
+			MessageBox.Show("TUTTO IN REGOLA");
+
+
+		}
+
+
+
+
+
+
+	}
+}
