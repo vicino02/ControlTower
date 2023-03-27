22bb9d37efa3582c51c1bf6416b355d33eb6c5ad
n.vicino <vicino@resolteam.com>
2023-03-27 09:34:32 +0200

    classe ip creata

---
+++ WindowsFormsApp1/FunzioniAuto.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+public class Class1
+{
+
+	int calcolo;
+	string ip_destination;
+	string ip_source;
+
+
+	public Class1()
+	{
+
+
+		public void PrelevaIp(string ips, string ipd)
+		{
+
+			ip_destination= ips;
+			ip_source= ipd;
+
+			MessageBox.Show("LE DUE SORGENTI prelevare risultano" + ip_destination"\n" + ip_source);
+
+
+		}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	}
+}
